53f03a569004f211f0e6371945b5aa7bac224c7e
Sandeep Bharati <iamsandeep196@gmai.com>
2026-05-21 19:27:19 +0530

    update

---
+++ frontend/src/pages/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+function Hero() {
+  return (
+    <>
+      <div
+        data-theme="forest"
+        className="min-h-screen bg-base-200 text-base-content p-10"
+      >
+        <h1 className="text-5xl font-bold">
+          MyQuote
+          <p className="mt-4 text-sm font-sans">Express what you think...</p>
+        </h1>
+
+        <p className="text-base-content/70 mt-4 text-sm">
+          Discover inspiring quotes and share your thoughts.
+        </p>
+
+        <button className="btn btn-neutral mt-6">Explore</button>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Hero;
