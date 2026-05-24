07a019a182a82b70601c0db60f372c55a61d0718
Sandeep Bharati <iamsandeep196@gmai.com>
2026-05-25 02:41:41 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -13,13 +13,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin : [
-
-        "http://localhost:5173",
-        "https://myquote-app-backend.vercel.app",
-              
-
-    ],
+    origin : "http://localhost:5173",
 
     credentials : true,
 }));
