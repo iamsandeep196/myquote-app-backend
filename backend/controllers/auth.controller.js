9973bceff8e216e2ba96920d6e28b1686dd3860f
Sandeep Bharati <iamsandeep196@gmai.com>
2026-07-22 10:55:08 +0530

    updated samesite

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -234,7 +234,7 @@ exports.loginUser = asyncHandler(async (req,res) => {
 
     res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
+        secure:true,
         // sameSite:"lax"
         sameSite:"none",
         maxAge:7*24*60*60*1000
